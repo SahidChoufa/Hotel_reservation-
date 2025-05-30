bf4c81a2371525fcffaace55070d587fb3249d8d
SahidChoufa <Sahidchoufa@gmail.com>
2025-05-30 09:58:20 +0200

    Fix Admin Component Files and Dependencies

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AdminComponent } from './admin.component';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { AdminComponent } from './admin.component';
 
 @NgModule({
   declarations: [
@@ -10,8 +9,10 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    FormsModule,
-    RouterModule
+    FormsModule
+  ],
+  exports: [
+    AdminComponent
   ]
 })
 export class AdminModule { }
